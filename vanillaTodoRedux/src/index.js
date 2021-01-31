5d53492f4f5a92bcb5e32811157e2b632cb868a9
hyesungoh <haesungoh414@gmail.com>
2021-01-31 16:44:33 +0900

    Delete ToDo with vanilla Redux

--- vanillaTodoRedux/src/index.js
+++ vanillaTodoRedux/src/index.js
@@ -7,37 +7,55 @@ const ul = document.querySelector(".js-ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addToDo = (text) => ({ type: ADD_TODO, text });
+
+const deleteToDo = (id) => ({ type: DELETE_TODO, id });
+
 const reducer = (state = [], action) => {
     switch (action.type) {
         case ADD_TODO:
-            return [...state, { text: action.text, id: Date.now() }];
+            return [{ text: action.text, id: Date.now() }, ...state];
         case DELETE_TODO:
-            return [];
+            return state.filter((toDo) => toDo.id !== action.id);
         default:
             return state;
     }
 };
 
-const store = createStore(reducer);
+const paintToDos = () => {
+    const toDos = store.getState();
+    ul.innerHTML = "";
+    toDos.forEach((toDo) => {
+        const li = document.createElement("li");
+        const btn = document.createElement("button");
+        btn.innerText = "DEL";
+        btn.addEventListener("click", dispatchDeleteToDo);
 
-const onChange = () => {
-    console.log(store.getState());
+        li.id = toDo.id;
+        li.innerText = toDo.text;
+        li.appendChild(btn);
+        ul.appendChild(li);
+    });
 };
 
-store.subscribe(onChange);
-
-const createTodo = (data) => {
-    const li = document.createElement("li");
-    li.innerText = data;
-    ul.appendChild(li);
+const dispatchAddToDo = (text) => {
+    store.dispatch(addToDo(text));
 };
 
+const dispatchDeleteToDo = (event) => {
+    const id = parseInt(event.target.parentNode.id);
+    store.dispatch(deleteToDo(id));
+};
 const handleSubmit = (event) => {
     event.preventDefault();
-    const data = input.value;
+    const text = input.value;
     input.value = "";
 
-    store.dispatch({ type: ADD_TODO, text: data });
+    dispatchAddToDo(text);
 };
+
+const store = createStore(reducer);
+
+store.subscribe(paintToDos);
 
 form.addEventListener("submit", handleSubmit);
